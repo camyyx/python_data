738d56ad119d6d5200488edc7be74181f518de03
camyyx <camyyx23@gmail.com>
2020-11-03 16:42:31 +0100

    tp4 et 5 sans reports

--- fastapi/main.py
+++ fastapi/main.py
@@ -54,4 +54,4 @@ def classiris(iris:Iris):
 
 
 if __name__ == '__main__' :
-    uvicorn.run(app,host="127.0.0.1",port="8000")
+    uvicorn.run(app,host="https://fastapimazouzi.herokuapp.com/",port="8000")
